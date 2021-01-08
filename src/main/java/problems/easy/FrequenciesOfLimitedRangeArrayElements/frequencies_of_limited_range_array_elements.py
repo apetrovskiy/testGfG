1f7a0f6a7b9d433e4be3f8c886090763e21c2880
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-08 08:28:44 +0300

    preparation

---
+++ src/main/java/problems/easy/FrequenciesOfLimitedRangeArrayElements/frequencies_of_limited_range_array_elements.py
@@ -0,0 +1,26 @@
+def frequencycount(A,N):
+    # code here
+
+
+
+'''
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+if __name__=="__main__":
+	T=int(input())
+	while(T>0):
+		N=int(input())
+		A=[int(x) for x in input().strip().split()]
+		frequencycount(A,N)
+		for i in range (len (A)):
+			print(A[i], end=" ")
+		print()
+		T-=1
+
+
+
+# } Driver Code Ends
+'''
