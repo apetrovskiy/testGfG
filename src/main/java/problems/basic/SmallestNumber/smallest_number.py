1b2c1b6047843fe5ef1c119595017deefabd8052
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-20 04:03:05 +0300

    preparation

---
+++ src/main/java/problems/basic/SmallestNumber/smallest_number.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+class Solution:
+    def smallestNumber (ob,S,D):
+        # code here 
+        return 1
+
+
+'''
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        
+        S,D=map(int,input().strip().split(" "))
+
+        ob = Solution()
+        print(ob.smallestNumber(S,D))
+# } Driver Code Ends
+'''
---
+++ src/test/java/problems/basic/SmallestNumber/test_smallest_number.py
@@ -0,0 +1,14 @@
+from src.main.java.problems.basic.SmallestNumber.smallest_number import Solution
+import pytest
+
+
+test_data = [
+    (9, 2, 18),
+    (20, 3, 299)
+]
+
+
+@pytest.mark.parametrize("sum_of_digits,number_of_digits,expected_result", test_data)
+def test_smallest_number(sum_of_digits: int, number_of_digits: int, expected_result: int):
+    assert expected_result == Solution().smallestNumber(
+        sum_of_digits, number_of_digits)
