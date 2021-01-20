354c2101feb995bc687766e9b308f70c2eaab363
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-20 05:20:07 +0300

    Python is done

--- src/main/java/problems/basic/SmallestNumber/smallest_number.py
+++ src/main/java/problems/basic/SmallestNumber/smallest_number.py
@@ -1,8 +1,21 @@
-#User function Template for python3
+# User function Template for python3
 class Solution:
-    def smallestNumber (ob,S,D):
-        # code here 
-        return 1
+    def smallestNumber(ob, S, D):
+        # code here
+        if S > D * 9 or S < 0:
+            return -1
+        minimum_remainder = 1
+        number_of_digits = D
+        result = []
+        while S > 0 and number_of_digits > 0:
+            current_item = 9 if (S - minimum_remainder) > 9 else S - minimum_remainder
+            S -= current_item
+            number_of_digits -= 1
+            result.append(current_item if number_of_digits > 0 else S + current_item)
+        result_value = 0
+        for index in range(len(result) - 1, -1, -1):
+            result_value += (result[index] * 10 ** (index))
+        return result_value
 
 
 '''
