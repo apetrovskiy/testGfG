f600ed45e8333e54d5a06b7d79cd1ead36a91a0b
Alexander Petrovskiy <aepetrovskiy@inno.tech>
2023-11-18 22:35:45 +0300

    updates & fixes in .NET

---
+++ src/main/java/problems/easy/HuffmanEncoding/HuffmanEncoding.cs
@@ -0,0 +1,13 @@
+// <copyright file="HuffmanEncoding.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System;
+
+public class HuffmanEncoding
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
